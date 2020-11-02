11a55b32fd77df4a6f08fadc91ced2eae4f9fb46
Teraaret <raiiliiat@yandex.ru>
2020-11-02 17:56:05 +0500

    Linter & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import cn from 'classnames'
+import React from 'react';
+import cn from 'classnames';
 
-import './custom.css'
-import s from './App.modules.scss'
-import image from './assets/images/pokemon.png'
+import s from './App.module.scss';
+import image from './assets/images/pokemon.png';
 
-import * as example from './example'
+import * as example from './example';
 
 const App = () => {
-    return (
-        <div className="wrapper">
-            <div className={s.block}>
-                <div className={s.header}>
-                    <p className={s.header__title}>
-                        Homework <span className={ cn(s.header__title__span, 'accent') }>#2</span>
-                    </p>
-                </div>
-                <div className={s.body}>
-                    <p className={s.body__line}><span className="accent">#1</span>: { example.concat('Hello', 'world') }</p>
-                    <p className={s.body__line}><span className="accent">#2</span>: { example.myHometask.howIDoIt }</p>
-                    <p className={s.body__line}><span className="accent">#3</span>: { example.summ }</p>
-                    <img className={s.body__image} src={image} alt="???"/>
-                </div>
-                <p className={s.copy}>&copy; React marathon PRO</p>
-            </div>
+  return (
+    <div className="wrapper">
+      <div className={s.block}>
+        <div className={s.header}>
+          <p className={s.header__title}>
+            Homework <span className={cn(s.header__title__span, 'accent')}>#2</span>
+          </p>
         </div>
-    )
-}
+        <div className={s.body}>
+          <p className={s.body__line}>
+            <span className="accent">#1</span>: {example.concat('Hello', 'world')}
+          </p>
+          <p className={s.body__line}>
+            <span className="accent">#2</span>: {example.myHometask.howIDoIt}
+          </p>
+          <p className={s.body__line}>
+            <span className="accent">#3</span>: {example.summ}
+          </p>
+          <img className={s.body__image} src={image} alt="???" />
+        </div>
+        <p className={s.copy}>&copy; React marathon PRO</p>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,61 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const NODE_ENV = process.env.NODE_ENV
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-    },
-    mode: NODE_ENV ? NODE_ENV : 'development',
-    entry: path.resolve(__dirname, 'src/index.tsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.[tj]sx?$/,
-                use: ['ts-loader']
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  mode: NODE_ENV ? NODE_ENV : 'development',
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-modules-typescript-loader?modules',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+                auto: /\.module\.\w+$/i,
+              },
             },
-            {
-                test: /\.(s*)css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                mode: 'local',
-                                localIdentName: '[name]__[local]__[hash:base64:5]',
-                                auto: /\.modules\.\w+$/i
-                            }
-                        }
-                    },
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/,
-                use: ['file-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'public/index.html')
-        })
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: ['file-loader'],
+      },
     ],
-    devServer: {
-        port: 3000,
-        // open: true,
-        hot: true,
-    },
-    devtool: 'source-map',
-}
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    // open: true,
+    hot: true,
+  },
+  devtool: 'source-map',
+};
